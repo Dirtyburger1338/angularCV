0ee718caf4a0b7df10e6eab85a9d6a0dbec8e723
Dirtyburger1338 <heaton1338@hotmail.com>
2018-02-04 13:09:51 +0100

    feaft

--- src/app/leftmenu/leftmenu.component.ts
+++ src/app/leftmenu/leftmenu.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from 'util';
+import { createWiresService } from 'selenium-webdriver/firefox';
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-leftmenu',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leftmenu.component.css']
 })
 export class LeftmenuComponent implements OnInit {
-
+  MenuElements: any;
   constructor() { }
 
   ngOnInit() {
   }
 
+  derp() {
+    this.MenuElements = $('.menu-wrapper > li > a');
+    this.MenuElements.removeClass('active');
+    this.MenuElements.removeClass('selected');
+  }
 }
